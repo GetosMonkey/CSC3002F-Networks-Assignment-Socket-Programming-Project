f4b7fb48ebba5fbe230d51c28cafc8d88e829ae1
GwilledCheez <yellowcarrot77@gmail.com>
2026-03-03 23:27:20 +0200

    updated protocal

--- protocol.py
+++ protocol.py
@@ -13,6 +13,27 @@ def encode_packet(sequence_number, body_bytes):
     header = struct.pack(HEADER_FORMAT, sequence_number, content_length)
     return header + body_bytes
 
-
 def decode_header(header_bytes):
-    return struct.unpack(HEADER_FORMAT, header_bytes)
+    return struct.unpack(...)
+
+def recv_exact(sock, n):
+    data = b''
+    while len(data) < n:
+        packet = sock.recv(n - len(data))
+        if not packet:
+            return None
+        data += packet
+    return data
+
+def receive_packet(sock):
+    header = recv_exact(sock, HEADER_SIZE)
+    if not header:
+        return None, None
+
+    sequence_number, content_length = decode_header(header)
+
+    body = recv_exact(sock, content_length)
+    if not body:
+        return None, None
+
+    return sequence_number, body
