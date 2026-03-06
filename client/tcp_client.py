e49698d696912b0e5d3ed59f61da4b6f9526a31a
GwilledCheez <yellowcarrot77@gmail.com>
2026-03-06 09:21:26 +0200

    Latest working version of tcp_client.py - from Saiyantha

--- client/tcp_client.py
+++ client/tcp_client.py
@@ -4,98 +4,101 @@ import threading
 SERVER_HOST = "localhost"
 SERVER_PORT = 12005
 
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect((SERVER_HOST, SERVER_PORT))
+print("Connected to server.")
+name = input("Enter chat username: ")
+print("Type messages in chat!")
 
 
-def start_client():
-    client_socket = socket(AF_INET, SOCK_STREAM)
-    client_socket.connect((SERVER_HOST, SERVER_PORT))
-
-    print("Connected to server.")
-
-    choice = show_menu()
-
-    authenticated = False
-
-    if choice =="1":
-        authenticated = login(client_socket)
-    elif choice =="2":
-        authenticated=sign_up(client_socket)
-    else:
-        print("Invalid Input!")
-
-    threading.Thread(target= receive_messages, args=(client_socket,), daemon=True).start()
+    
 
 
-    show_commands()
+   # choice = show_menu()
 
-    while authenticated:
-        message = input("Select option from command list (or 'exit'): ")
+   # authenticated = True
 
-        if message.lower() == "exit":
-            break
+    #if choice =="1":
+     #   authenticated = login(client_socket)
+    #elif choice =="2":
+    #    authenticated=sign_up(client_socket)
+    ##else:
+    #    print("Invalid Input!")
 
-        client_socket.send(message.encode())
+   # show_commands()
 
-    client_socket.close()
+  
 
 def receive_messages(client_socket):
     while True:
-        data = client_socket.recv(1024).decode()
-        print(data)
+        try:
+            message = client_socket.recv(1024).decode()
+            if not message:
+                break
+            print(message)
+            
+   
+        except:
+            print("An error occurred!")
+            client_socket.close()
+            break
+
+def send_messages(client_socket):
+    while True:
+        message = input()
+        if message.lower() == "quit":
+            client_socket.close()
+            break
+        message = name + ": " + message 
+        client_socket.send(message.encode())
         
 
-def authenticate(username,password, client_socket):
-    message_string="Authenticate/"+username+"/"+password
-    client_socket.send(message_string.encode())
+#def authenticate(username,password, client_socket):
+    #message_string="Authenticate/"+username+"/"+password
+   # client_socket.send(message_string.encode())
 
 
 
-def login(client_socket):
-    username = input("Enter username: ")
-    password = input("Enter password:  ")
-    authenticate(username,password,client_socket)
-    response = client_socket.recv(1024).decode().strip()
-    if response == "SUCCESS":
-        authenticated = True
-    else:
-        authenticated = False
+#def login(client_socket):
+   # username = input("Enter username: ")
+    #password = input("Enter password:  ")
+    #authenticate(username,password,client_socket)
+    #response= client_socket.recv(1024).decode)
+    #if response ="SUCCESS":
+    #    authenticated =True
+    #else authenticated =False
 
-    return authenticated
 
-def show_commands():
-    print("Select a command from the list below!") ## This method prompts the user to Login or sign-up
-    print("1. Private Message") ## Login for existing users
-    print("2. Message Group") ## sign-up for new users
-    print("3. Create New Group")
-    print("4. Join Group")
+    #return authenticated
+
+#def show_commands():
+   ### print("Select a command from the list below!") ## This method prompts the user to Login or sign-up
+ #   print("1. Private Message") ## Login for existing users
+  #  print("2. Message Group") ## sign-up for new users
+   # print("3. Create New Group")
+    #print("4. Join Group")
     
 
 
-def show_menu():
-    print("Welcome to ChatApp!") ## This method prompts the user to Login or sign-up
-    print("1. Login") ## Login for existing users
-    print("2. Sign-up") ## sign-up for new users
-    reply = input("Select option: ")
-    return reply
+#def show_menu():
+   # print("Welcome to ChatApp!") ## This method prompts the user to Login or sign-up
+   ## print("1. Login") ## Login for existing users
+  ##  print("2. Sign-up") ## sign-up for new users
+  #  reply = input("Select option: ")
+  #  return reply
 
  
-def sign_up(client_socket):
-    username = input("Enter a username: ")
-    password = input("Enter a password: ")
-    message_string = "NewUser" + "/" + username + "/" + password
-    client_socket.send(message_string.encode())
-    
-    response = client_socket.recv(1024).decode().strip()
-    if response == "SUCCESS":
-        return True
-    return False
-
-def main():
-    start_client()
+#def sign_up(client_socket):
+   # username=input("Enter a username: ")
+    #password=input("Enter a password: ")
+   ## message_string=input("NewUser"+"/"+username+"/"+password)
+   ## client_socket.send(message_string.encode())
+  #  return valid
 
 
 
 
 
-if __name__ == "__main__":
-    main()
+
+threading.Thread(target= receive_messages, args=(client_socket,)).start()
+threading.Thread(target= send_messages, args=(client_socket,)).start()
